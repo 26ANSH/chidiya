6b4742227562aa9e70f7b8920f99fdd16e7d7142
26ANSH <anshvidyabhanu8@gmail.com>
2021-04-05 11:52:14 +0530

    ok

--- index.php
+++ index.php
@@ -7,9 +7,6 @@
 
 <?php include_once "header.php"; ?>
 <style media="screen">
-body{
-    background: lightblue;
-}
 .button {
   border-radius: 4px;
   background-color: yellow;
@@ -17,7 +14,7 @@ body{
   color: black;
   text-align: center;
   font-size: 24px;
-  padding: 20px;
+  padding: 10px;
   width: 150px;
   transition: all 0.5s;
   cursor: pointer;
@@ -58,7 +55,7 @@ body{
       <lottie-player src="https://assets10.lottiefiles.com/private_files/lf30_TBKozE.json"  background="#FF0000"  speed="1"  style="width: 375px; height: 300px;"  loop  autoplay></lottie-player>
       <header align="center">Welcome !!!</header>
       <br>
-      Chidiya is a free open source safe chatting interface developed by Ansh Maanas Yashraj Deepak Lakshay
+      Chidiya is a free open source safe chatting interface developed by Group-2
       <div class="" align="center">
         <button class="button" onclick="window.location.href='signup.php'"><span>SignUp</span></button>
         <button class="button" onclick="window.location.href='login.php'"><span>LogIn</span></button>
