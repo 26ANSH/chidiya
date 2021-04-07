e9ab7e27f4674b09857ab662cce94525cf923e9d
26ANSH <anshvidyabhanu8@gmail.com>
2021-04-07 10:47:15 +0530

    database default file

--- chatapp.sql
+++ chatapp.sql
@@ -1,24 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.9.5
--- https://www.phpmyadmin.net/
---
--- Host: localhost:8889
--- Generation Time: Apr 06, 2021 at 08:06 PM
--- Server version: 5.7.30
--- PHP Version: 7.4.9
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
---
--- Database: `chatapp`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `messages`
---
 
 CREATE TABLE `messages` (
   `msg_id` int(11) NOT NULL,
@@ -27,12 +8,6 @@ CREATE TABLE `messages` (
   `msg` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `unique_id` int(255) NOT NULL,
@@ -40,41 +15,25 @@ CREATE TABLE `users` (
   `lname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `img` varchar(255) NOT NULL DEFAULT 'default.png',
+  `img` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL,
   `activationcode` varchar(255) NOT NULL,
   `verify` int(11) NOT NULL,
   `theme` varchar(255) NOT NULL DEFAULT 'lightblue'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `messages`
---
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`msg_id`);
 
---
--- Indexes for table `users`
---
+
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `messages`
---
 ALTER TABLE `messages`
   MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `users`
---
+
 ALTER TABLE `users`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
