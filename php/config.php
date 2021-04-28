6122771a1e6c093f409b25359304cfc9c8a337c1
26ANSH <anshvidyabhanu8@gmail.com>
2021-04-28 13:33:14 +0530

    bugs fixed

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php
   $hostname = "localhost";
   $username = "root";
-  $password = "root";
+  $password = "19bcs@CUCHD";
   $dbname = "chatapp";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
